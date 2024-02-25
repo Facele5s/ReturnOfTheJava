b685c5e5a22495e80f400253073c8ecfa9add895
Facele5s <otherworldly.elban@gmail.com>
2024-02-25 20:57:54 +0300

    Added web clients

---
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -0,0 +1,16 @@
+package edu.java;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+public class LinkUpdaterScheduler {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Scheduled(fixedDelayString = "${app.scheduler.interval}")
+    public void update() {
+        LOGGER.info("Updated");
+    }
+}
