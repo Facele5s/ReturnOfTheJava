946055dfadf49c6edd1625e8ad1da2d7a6acaed6
Facele5s <otherworldly.elban@gmail.com>
2024-03-16 22:20:36 +0300

    Scheduler fixed

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -4,8 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @EnableScheduling
+@Component
 public class LinkUpdaterScheduler {
     private static final Logger LOGGER = LogManager.getLogger();
 
