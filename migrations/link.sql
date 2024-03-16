caae7a3d3f303d9b4a2559613ab71804159862a5
Facele5s <otherworldly.elban@gmail.com>
2024-03-16 18:33:50 +0300

    Updated db structure

--- migrations/link.sql
+++ migrations/link.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS Link
 (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
+    chat_id BIGINT NOT NULL,
     url TEXT NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE,
+    checked_at TIMESTAMP WITH TIME ZONE,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (chat_id) REFERENCES chat(id)
 );
