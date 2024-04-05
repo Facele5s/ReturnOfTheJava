6c0ac7eb5288d2c79a5623cd973e4a6393b87ed8
Facele5s <otherworldly.elban@gmail.com>
2024-04-05 23:20:55 +0300

    Codestyle fix

---
+++ scrapper/src/main/java/edu/java/client/github/model/Commit.java
@@ -0,0 +1,26 @@
+package edu.java.client.github.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.OffsetDateTime;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Commit {
+    private String sha;
+    private CommitInfo commit;
+
+    public OffsetDateTime getDate() {
+        return commit.getAuthor().getDate();
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- scrapper/src/main/java/edu/java/client/github/model/Pull.java
+++ scrapper/src/main/java/edu/java/client/github/model/Pull.java
@@ -1,11 +1,11 @@
-package edu.java.client.github;
+package edu.java.client.github.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.OffsetDateTime;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
-import java.time.OffsetDateTime;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
