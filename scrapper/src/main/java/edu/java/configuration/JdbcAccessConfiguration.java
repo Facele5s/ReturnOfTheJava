50a834e725f80a51f006caa4b4aca48f877309aa
Facele5s <otherworldly.elban@gmail.com>
2024-04-16 02:32:39 +0300

    Updated JPA, JDBC access configuration

--- scrapper/src/main/java/edu/java/configuration/JdbcAccessConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/JdbcAccessConfiguration.java
@@ -1,10 +1,22 @@
 package edu.java.configuration;
 
 import edu.java.dao.JdbcChatDao;
+import edu.java.dao.JdbcGithubCommitDao;
+import edu.java.dao.JdbcGithubPullDao;
+import edu.java.dao.JdbcGithubReleaseDao;
+import edu.java.dao.JdbcGithubRepositoryDao;
 import edu.java.dao.JdbcLinkDao;
 import edu.java.service.ChatService;
+import edu.java.service.GithubCommitService;
+import edu.java.service.GithubPullService;
+import edu.java.service.GithubReleaseService;
+import edu.java.service.GithubRepositoryService;
 import edu.java.service.LinkService;
 import edu.java.service.jdbc.JdbcChatService;
+import edu.java.service.jdbc.JdbcGithubCommitService;
+import edu.java.service.jdbc.JdbcGithubPullService;
+import edu.java.service.jdbc.JdbcGithubReleaseService;
+import edu.java.service.jdbc.JdbcGithubRepositoryService;
 import edu.java.service.jdbc.JdbcLinkService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -22,5 +34,25 @@ public class JdbcAccessConfiguration {
     @Bean
     public LinkService linkService(JdbcLinkDao jdbcLinkDao) {
         return new JdbcLinkService(jdbcLinkDao);
+    }
+
+    @Bean
+    public GithubRepositoryService githubRepositoryService(JdbcGithubRepositoryDao repositoryDao) {
+        return new JdbcGithubRepositoryService(repositoryDao);
+    }
+
+    @Bean
+    public GithubCommitService githubCommitService(JdbcGithubCommitDao commitDao) {
+        return new JdbcGithubCommitService(commitDao);
+    }
+
+    @Bean
+    public GithubPullService githubPullService(JdbcGithubPullDao pullDao) {
+        return new JdbcGithubPullService(pullDao);
+    }
+
+    @Bean
+    public GithubReleaseService githubReleaseService(JdbcGithubReleaseDao releaseDao) {
+        return new JdbcGithubReleaseService(releaseDao);
     }
 }
--- scrapper/src/main/java/edu/java/configuration/JpaAccessConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/JpaAccessConfiguration.java
@@ -1,10 +1,22 @@
 package edu.java.configuration;
 
 import edu.java.repository.JpaChatRepository;
+import edu.java.repository.JpaGithubCommitRepository;
+import edu.java.repository.JpaGithubPullRepository;
+import edu.java.repository.JpaGithubReleaseRepository;
+import edu.java.repository.JpaGithubRepoRepository;
 import edu.java.repository.JpaLinkRepository;
 import edu.java.service.ChatService;
+import edu.java.service.GithubCommitService;
+import edu.java.service.GithubPullService;
+import edu.java.service.GithubReleaseService;
+import edu.java.service.GithubRepositoryService;
 import edu.java.service.LinkService;
 import edu.java.service.jpa.JpaChatService;
+import edu.java.service.jpa.JpaGithubCommitService;
+import edu.java.service.jpa.JpaGithubPullService;
+import edu.java.service.jpa.JpaGithubReleaseService;
+import edu.java.service.jpa.JpaGithubRepositoryService;
 import edu.java.service.jpa.JpaLinkService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -22,5 +34,25 @@ public class JpaAccessConfiguration {
     @Bean
     public LinkService linkService(JpaLinkRepository jpaLinkRepository, JpaChatRepository jpaChatRepository) {
         return new JpaLinkService(jpaLinkRepository, jpaChatRepository);
+    }
+
+    @Bean
+    public GithubRepositoryService githubRepositoryService(JpaGithubRepoRepository repoRepository) {
+        return new JpaGithubRepositoryService(repoRepository);
+    }
+
+    @Bean
+    public GithubCommitService githubCommitService(JpaGithubCommitRepository commitRepository) {
+        return new JpaGithubCommitService(commitRepository);
+    }
+
+    @Bean
+    public GithubPullService githubPullService(JpaGithubPullRepository pullRepository) {
+        return new JpaGithubPullService(pullRepository);
+    }
+
+    @Bean
+    public GithubReleaseService githubReleaseService(JpaGithubReleaseRepository releaseRepository) {
+        return new JpaGithubReleaseService(releaseRepository);
     }
 }
